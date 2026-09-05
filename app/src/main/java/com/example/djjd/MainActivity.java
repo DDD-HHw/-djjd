d24a77c8c06b6c5fa60d92913b66da870d158366
DDD-HHw <Hhw8001023a@qq.com>
2026-09-05 08:43:28 +0000

    初始化安卓项目

---
+++ app/src/main/java/com/example/djjd/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.djjd;
+import android.app.Activity; import android.os.Bundle;
+public class MainActivity extends Activity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(android.R.layout.activity_list_item);
+    }
+}
